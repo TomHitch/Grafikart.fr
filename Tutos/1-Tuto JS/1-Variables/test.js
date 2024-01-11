c7fe2ea44fbb8cc661dc55f106e3a110cd0847a7
Thomas MESSIN <89847025+TomHitch@users.noreply.github.com>
2024-01-11 12:49:08 +0100

    first commit

---
+++ Tutos/1-Tuto JS/1-Variables/test.js
@@ -0,0 +1,24 @@
+const a = 15 // constante de type number
+let b = "bonjour" // variable de type string
+const c = "la famille" // constante de type string
+const isMajeur = false // constante de type booléen faux
+let isMajeur = null // variable de type booléen nul
+let notes = [    // tableau de valeurs, ici 3 notes
+    10,
+    12,
+    11,
+]
+let peronnes = {    // objet
+    prenom : "Thomas",
+    nom : "Messin",
+    age : 36,
+    notes : [
+        15,
+        18,
+        12,
+    ],
+    travail : {
+        fonction : "développeurs web",
+        nHeures : undefined,
+    }
+}
