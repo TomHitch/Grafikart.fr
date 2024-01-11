157f90f1044ea24f4a8d0f439054404f1d74595b
Thomas MESSIN <89847025+TomHitch@users.noreply.github.com>
2024-01-11 13:34:19 +0100

    modification du dossier

--- Tutos/1-Tuto JS/1-Variables/test.js
+++ Tutos/1-Tuto JS/1-Variables/test.js
@@ -1,14 +1,14 @@
 const a = 15 // constante de type number
 let b = "bonjour" // variable de type string
 const c = "la famille" // constante de type string
-const isMajeur = false // constante de type booléen faux
-let isMajeur = null // variable de type booléen nul
+//const isMajeur = false // constante de type booléen faux
+//let isMajeur = null // variable de type booléen nul
 let notes = [    // tableau de valeurs, ici 3 notes
     10,
     12,
     11,
 ]
-let peronnes = {    // objet
+let personnes = {    // objet
     prenom : "Thomas",
     nom : "Messin",
     age : 36,
@@ -22,3 +22,6 @@ let peronnes = {    // objet
         nHeures : undefined,
     }
 }
+const debut = "Bonjour, "
+const fin = "je m'appelle Thomas"
+console.log(debut + fin) // Concaténation de texte
